e91056bed3434d4d0392265c863b7cbca7029646
ssdavidson <ssdavidson@ucdavis.edu>
2020-11-13 23:59:41 +0000

    update files

---
+++ bert_classifier/load_data.py
@@ -0,0 +1,10 @@
+# Load the dataset into a pandas dataframe.
+import pandas as pd
+
+def get_data(csv_file):
+    df = pd.read_csv(csv_file, delimiter=',', header=0)
+    labels = df.polarization.values
+    comments = df.text.values
+    print('Number of training sentences: {:,}\n'.format(df.shape[0]))
+    return comments, labels
+
