2cc563a84cf7360a7eb474b1fad6b6da5b40d00c
ssdavidson <ssdavidson@ucdavis.edu>
2021-05-02 12:45:34 -0700

    update load data:

--- bert_classifier/load_data_run.py
+++ bert_classifier/load_data_run.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 def get_data(csv_file):
-    df = pd.read_csv(csv_file, delimiter=',', header=0).dropna()
+    df = pd.read_csv(csv_file, delimiter=',', header=0)
 
     comments = df.text.values
     print('Number of training sentences: {:,}\n'.format(df.shape[0]))
