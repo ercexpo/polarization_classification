561abdaddfe37a216bb8ec352ed60f8c5a185b09
ssdavidson <ssdavidson@ucdavis.edu>
2021-06-10 13:24:24 -0700

    add test script

---
+++ bert_classifier/test_bert_model.py
@@ -0,0 +1,173 @@
+import torch
+import pandas as pd
+from transformers import BertTokenizer
+import load_data
+import sys, time, datetime, random
+#from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from transformers import BertForSequenceClassification, AdamW, BertConfig
+from transformers import get_linear_schedule_with_warmup
+import numpy as np
+import krippendorff
+
+# If there's a GPU available...
+if torch.cuda.is_available():
+
+    # Tell PyTorch to use the GPU.
+    device = torch.device("cuda")
+
+    print('There are %d GPU(s) available.' % torch.cuda.device_count())
+
+    print('We will use the GPU:', torch.cuda.get_device_name(0))
+
+# If not...
+else:
+    print('No GPU available, using the CPU instead.')
+    device = torch.device("cpu")
+
+#load comments and labels from the input tsv
+val_comments, val_labels = load_data.get_data(sys.argv[2])
+
+# Load the BERT tokenizer.
+print('Loading BERT tokenizer...')
+#tokenizer = BertTokenizer.from_pretrained('bert-base-cased', do_lower_case=False)
+tokenizer = BertTokenizer.from_pretrained(sys.argv[1])
+
+#encode inputs using BERT tokenizer
+val_input_ids = []
+
+for comment in val_comments:
+    encoded_comment = tokenizer.encode(comment, add_special_tokens = True, max_length=512,pad_to_max_length=True,truncation=True)
+    val_input_ids.append(encoded_comment)
+
+#define attention masks: if 0 it's a PAD, set to 0; else set to 1
+val_attention_masks = []
+
+for sent in val_input_ids:
+    att_mask = [int(token_id > 0) for token_id in sent]
+    val_attention_masks.append(att_mask)
+
+val_labels = val_labels.astype(np.int)
+
+# Convert all inputs and labels into torch tensors, the required datatype
+# for our model.
+validation_inputs = torch.tensor(val_input_ids)
+
+validation_labels = torch.tensor(val_labels)
+
+validation_masks = torch.tensor(val_attention_masks)
+
+# The DataLoader needs to know our batch size for training, so we specify it
+# here.
+# For fine-tuning BERT on a specific task, the authors recommend a batch size of
+# 16 or 32.
+
+batch_size = 6
+
+# Create the DataLoader for our validation set.
+validation_data = TensorDataset(validation_inputs, validation_masks, validation_labels)
+validation_sampler = SequentialSampler(validation_data)
+validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, batch_size=batch_size)
+
+# Load BertForSequenceClassification, the pretrained BERT model with a single
+# linear classification layer on top.
+model = BertForSequenceClassification.from_pretrained(
+    sys.argv[1], # Use the 12-layer BERT model, with an uncased vocab.
+    num_labels = 2, # The number of output labels--2 for binary classification.
+                    # You can increase this for multi-class tasks.
+    output_attentions = False, # Whether the model returns attentions weights.
+    output_hidden_states = False, # Whether the model returns all hidden-states.
+)
+
+# Tell pytorch to run this model on the GPU.
+model.cuda()
+
+def flat_accuracy(preds, labels):
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+def format_time(elapsed):
+    '''
+    Takes a time in seconds and returns a string hh:mm:ss
+    '''
+    # Round to the nearest second.
+    elapsed_rounded = int(round((elapsed)))
+
+    # Format as hh:mm:ss
+    return str(datetime.timedelta(seconds=elapsed_rounded))
+
+# Set the seed value all over the place to make this reproducible.
+seed_val = 42
+
+random.seed(seed_val)
+np.random.seed(seed_val)
+torch.manual_seed(seed_val)
+torch.cuda.manual_seed_all(seed_val)
+
+print("Running Validation...")
+
+t0 = time.time()
+
+# Put the model in evaluation mode--the dropout layers behave differently
+# during evaluation.
+model.eval()
+
+# Tracking variables
+eval_loss, eval_accuracy = 0, 0
+nb_eval_steps, nb_eval_examples = 0, 0
+
+y_pred, y_true = [],[]
+
+# Evaluate data for one epoch
+for batch in validation_dataloader:
+
+    # Add batch to GPU
+    batch = tuple(t.to(device) for t in batch)
+
+    # Unpack the inputs from our dataloader
+    b_input_ids, b_input_mask, b_labels = batch
+
+    # Telling the model not to compute or store gradients, saving memory and
+    # speeding up validation
+    with torch.no_grad():
+
+        # Forward pass, calculate logit predictions.
+        # This will return the logits rather than the loss because we have
+        # not provided labels.
+        # token_type_ids is the same as the "segment ids", which
+        # differentiates sentence 1 and 2 in 2-sentence tasks.
+        # The documentation for this `model` function is here:
+        # https://huggingface.co/transformers/v2.2.0/model_doc/bert.html#transformers.BertForSequenceClassification
+        outputs = model(b_input_ids,
+                        token_type_ids=None,
+                        attention_mask=b_input_mask)
+
+    # Get the "logits" output by the model. The "logits" are the output
+    # values prior to applying an activation function like the softmax.
+    logits = outputs[0]
+
+    # Move logits and labels to CPU
+    logits = logits.detach().cpu().numpy()
+    label_ids = b_labels.to('cpu').numpy()
+
+    # add labels and logits to list for F1 calculation:
+    y_pred.extend(np.argmax(logits, axis=1).flatten())
+    y_true.extend(label_ids.flatten())
+
+    # Calculate the accuracy for this batch of test sentences.
+    tmp_eval_accuracy = flat_accuracy(logits, label_ids)
+
+    # Accumulate the total accuracy.
+    eval_accuracy += tmp_eval_accuracy
+
+    # Track the number of batches
+    nb_eval_steps += 1
+
+# Report the final accuracy for this validation run.
+print("  Accuracy: {0:.2f}".format(eval_accuracy/nb_eval_steps))
+print("  Validation took: {:}".format(format_time(time.time() - t0)))
+print("  Krippendorff: {0:.2f}".format(krippendorff.alpha(reliability_data=[y_pred,y_true])))
+print(classification_report(y_pred,y_true))
